0df7f86f1ad04cc3fa6d45d2c03da1fa9e02c76b
Zaki Ramadan <zakiramadan019@gmail.com>
2024-04-07 14:25:08 +0700

    first commit

---
+++ My Bio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata Mahasiswa</title>
+</head>
+<body>
+
+<?php
+// Definisikan data mahasiswa
+$nama = "Zaki Tirta Ramadan";
+$nim = "09030582226024";
+$kelas = "TK4B";
+$mata_kuliah = "Praktikum Jaringan Komputer";
+
+// Tampilkan biodata mahasiswa
+echo "<h2>Biodata Mahasiswa</h2>";
+echo "<p>Nama: $nama</p>";
+echo "<p>NIM: $nim</p>";
+echo "<p>Kelas: $kelas</p>";
+echo "<p>Mata Kuliah: $mata_kuliah</p>";
+?>
+
+</body>
+</html>
