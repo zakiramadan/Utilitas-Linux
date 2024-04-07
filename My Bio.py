4b76681d083ffb9ae9f10a4a200f55a071caac46
Zaki Ramadan <zakiramadan019@gmail.com>
2024-04-07 14:40:47 +0700

    File changes

---
+++ My Bio.py
@@ -0,0 +1,12 @@
+# Informasi biodata mahasiswa
+nama = "Zaki Tirta Ramadan"
+nim = "09030582226024"
+kelas = "TK4B"
+mata_kuliah = "Praktikum Jaringan Komputer"
+
+# Menampilkan biodata mahasiswa ke konsol
+print("Biodata Mahasiswa")
+print("Nama:", nama)
+print("NIM:", nim)
+print("Kelas:", kelas)
+print("Mata Kuliah:", mata_kuliah)
